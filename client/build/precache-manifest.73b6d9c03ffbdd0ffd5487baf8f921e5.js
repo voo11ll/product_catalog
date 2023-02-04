925d763937523870c66f307b30ca7f5d9cd9a63f
vrungel <arsenyan.m.i20@gmail.com>
2023-02-04 21:03:03 +0300

    product

---
+++ client/build/precache-manifest.73b6d9c03ffbdd0ffd5487baf8f921e5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8bf734b94c508f2d595cbec1091441b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dda0784822b91e1d92c",
+    "url": "/static/css/main.1edf2ec6.chunk.css"
+  },
+  {
+    "revision": "38e6e295405c1c8d5df2",
+    "url": "/static/js/2.25f416f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.25f416f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dda0784822b91e1d92c",
+    "url": "/static/js/main.c726ff27.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/static/media/cart.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/static/media/close.2c5552a1.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/static/media/menu.b3f49afd.svg"
+  }
+]);
